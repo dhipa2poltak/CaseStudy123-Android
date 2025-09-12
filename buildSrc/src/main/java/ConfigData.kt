ff36cce6a7e7bbc76d49e3b3999f769f97e31346
Dhipa Poltak <dhipa2poltak@gmail.com>
2025-09-12 21:11:37 +0700

    increasing app version to 4 (1.4) and releasing on GitHub.

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -4,10 +4,10 @@ object ConfigData {
   const val compileSdkVersion = 34
   const val minSdkVersion = 21
   const val targetSdkVersion = 34
-  const val versionCode = 3
-  const val versionName = "1.3"
-  const val versionCodeDev = 3
-  const val versionNameDev = "1.3"
+  const val versionCode = 4
+  const val versionName = "1.4"
+  const val versionCodeDev = 4
+  const val versionNameDev = "1.4"
   val sourceCompatibilityVersion = JavaVersion.VERSION_17
   val targetCompatibilityVersion = JavaVersion.VERSION_17
   const val jvmTargetVersion = "17"
